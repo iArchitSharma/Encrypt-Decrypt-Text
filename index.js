2312782910b67f3f659775d42c2f98060515782c
Archit Sharma <74408634+iArchitSharma@users.noreply.github.com>
2021-06-18 13:15:00 +0700

    js added for copy buttons

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ function decrypt(message = '', key = '') { //This function will take message and
 function AesEncrypt() { //This function is executed using Encrypt button
     var valid = document.forms["myForm"]["inputText"].value;
     if (valid == null || valid == "") {
-        document.getElementById("demo1").innerHTML = '<p style="color:red; font-size:15px; text-align:center;">Please enter a text</p>';
+        document.getElementById("Derror").innerHTML = '<p style="color:red; font-size:15px; text-align:center;">Please enter a text</p>';
     } else {
         const text = document.getElementById('inputText').value;
         const password = document.getElementById('inputPassword').value;
         var x = encrypt(text, password);
-        document.getElementById("demo1").innerHTML = x;
+        document.getElementById('demo1').value = x;
     }
 
 }
@@ -26,11 +26,37 @@ function AesEncrypt() { //This function is executed using Encrypt button
 function AesDecrypt() { //This function is executed using Decrypt button
     var valid = document.forms["myForm"]["inputText"].value;
     if (valid == null || valid == "") {
-        document.getElementById("demo2").innerHTML = '<p style="color:red; font-size:15px; text-align:center;">Paste encrypted text</p>';
+        document.getElementById("Eerror").innerHTML = '<p style="color:red; font-size:15px; text-align:center;">Paste encrypted text</p>';
     } else {
         const text1 = document.getElementById('inputText').value;
         const password2 = document.getElementById('inputPassword').value;
         var x1 = decrypt(text1, password2);
-        document.getElementById("demo2").innerHTML = x1;
+        document.getElementById("demo2").value = x1;
     }
+}
+
+function myCopy1() { //Function to copy text from input field of encrypted text
+    /* Get the text field */
+    var copyText = document.getElementById("demo1");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+}
+
+function myCopy2() { //Function to copy text from input field of decrypted text
+    /* Get the text field */
+    var copyText = document.getElementById("demo2");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+
+
 }
